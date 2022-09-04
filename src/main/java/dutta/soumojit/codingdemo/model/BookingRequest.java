fea84636bf6930b6decff8ea69c5e4180c2c004a
soumojitd22 <soumojitd22@gmail.com>
2022-09-04 10:34:33 +0530

    Fixed nested object validation issue

--- src/main/java/dutta/soumojit/codingdemo/model/BookingRequest.java
+++ src/main/java/dutta/soumojit/codingdemo/model/BookingRequest.java
@@ -4,6 +4,7 @@ import dutta.soumojit.codingdemo.entity.Hiker;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.Valid;
 import javax.validation.constraints.NotEmpty;
 import java.util.List;
 
@@ -12,6 +13,7 @@ import java.util.List;
 public class BookingRequest {
     private int trailId;
 
+    @Valid
     @NotEmpty(message = "At least one hiker is required")
     private List<Hiker> hikers;
 }
