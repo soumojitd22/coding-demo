3b2232ee570544370d97e54df003f139203620c6
soumojitd22 <soumojitd22@gmail.com>
2022-12-22 13:23:34 +0530

    Junit of main class to increase code coverage

---
+++ src/test/java/dutta/soumojit/codingdemo/CodingDemoApplicationTest.java
@@ -0,0 +1,13 @@
+package dutta.soumojit.codingdemo;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CodingDemoApplicationTest {
+    @Test
+    void testMain() {
+        CodingDemoApplication.main(new String[] {});
+        assertTrue(true);
+    }
+}
